1097d979495ad6a65404bd652268b26677dede30
tahasheikh <155918656+tahasheikk@users.noreply.github.com>
2025-01-07 01:53:15 +0330

    doublelinkedkist
    doublelinkedkist

--- Program.cs
+++ Program.cs
@@ -1,70 +1,71 @@
 ﻿using System;
 
-namespace linklist
+namespace DoublyLinkedList
 {
-    // Node class representing an element in the linked list
-    public class node
+    // Node class representing an element in the doubly linked list
+    public class Node
     {
         public int Data; // Data of the node
-        public node next; // Pointer to the next node
+        public Node Next; // Pointer to the next node
+        public Node Befor; // Pointer to the befor node ( 'Befor')
 
-        public node(int data)
+        public Node(int data)
         {
             Data = data; // Initialize the data
-            next = null; // Initially, the pointer to the next node is null
+            Next = null; // Initially, the pointer to the next node is null
+            Befor = null; // Initially, the pointer to the  node is null
         }
     }
 
-    // Circular linked list class
-    public class circlelinkedlist
+    // Doubly linked list class
+    public class DoublyLinkedList
     {
-        private node head; // Pointer to the first node
+        private Node head; // Pointer to the first node
         private int size; // Size of the list
 
-        public circlelinkedlist()
+        public DoublyLinkedList()
         {
             head = null; // Initially, the list is empty
             size = 0; // Size of the list is zero
         }
 
         // Method to insert a node at a specific index
-        public void INsertAtIndex(int data, int index)
+        public void InsertAtIndex(int data, int index)
         {
             if (index < 0 || index > size)
             {
-                Console.WriteLine("out of range or empty");
+                Console.WriteLine("Out of range or empty");
                 return; // If the index is invalid, exit the method
             }
 
-            node newnode = new node(data); // Create a new node
+            Node newNode = new Node(data); // Create a new node
             if (index == 0) // If the index is 0
             {
                 if (head == null) // If the list is empty
                 {
-                    head = newnode; // The new node becomes the head
-                    newnode.next = head; // Point to itself (circular)
+                    head = newNode; // The new node becomes the head
                 }
                 else
                 {
-                    node tail = head;
-                    while (tail.next != head) // Find the last node
-                    {
-                        tail = tail.next;
-                    }
-                    tail.next = newnode; // Link the last node to the new node
-                    newnode.next = head; // New node points to head
-                    head = newnode; // Update head to the new node
+                    newNode.Next = head; // New node points to the current head
+                    head.Befor = newNode; // Current head's previous points to new node
+                    head = newNode; // Update head to the new node
                 }
             }
             else // If the index is not 0
             {
-                node newp = head;
+                Node current = head;
                 for (int i = 0; i < index - 1; i++) // Move to the node before the index
                 {
-                    newp = newp.next;
+                    current = current.Next;
                 }
-                newnode.next = newp.next; // Link new node to the next node
-                newp.next = newnode; // Link previous node to new node
+                newNode.Next = current.Next; // Link new node to the next node
+                if (current.Next != null) // If not inserting at the end
+                {
+                    current.Next.Befor = newNode; // Link next node's previous to new node
+                }
+                current.Next = newNode; // Link previous node to new node
+                newNode.Befor = current; // Link new node's previous to current
             }
             size++; // Increment the size of the list
         }
@@ -72,30 +73,30 @@ namespace linklist
         // Method to insert at the end of the list
         public void InsertAtEnd(int data)
         {
-            INsertAtIndex(data, size); // Insert at index equal to the size of the list
+            InsertAtIndex(data, size); // Insert at index equal to the size of the list
         }
 
         // Method to insert at the beginning of the list
         public void InsertAtBegin(int data)
         {
-            INsertAtIndex(data, 0); // Insert at index 0
+            InsertAtIndex(data, 0); // Insert at index 0
         }
 
         // Method to update the data of a node at a specific index
-        public void UpdateNode(int data, int index)//update data of node
+        public void UpdateNode(int data, int index) // Update data of node
         {
             if (index < 0 || index >= size) // Check for valid index
             {
-                Console.WriteLine("out of range or empty");
+                Console.WriteLine("Out of range or empty");
                 return; // If the index is invalid, exit the method
             }
 
-            node newp = head;
+            Node current = head;
             for (int i = 0; i < index; i++) // Move to the node at the specified index
             {
-                newp = newp.next;
+                current = current.Next;
             }
-            newp.Data = data; // Update the data of the node
+            current.Data = data; // Update the data of the node
         }
 
         // Method to remove a node at a specific index
@@ -103,47 +104,46 @@ namespace linklist
         {
             if (index < 0 || index >= size) // Check for valid index
             {
-                Console.WriteLine("out of range");
+                Console.WriteLine("Out of range");
                 return -1; // Return -1 for invalid index
             }
 
-            node remove; // Node to be removed
-            int removedata; // Data of the removed node
+            Node remove; // Node to be removed
+            int removedData; // Data of the removed node
             if (index == 0) // If the head needs to be removed
             {
                 remove = head; // Store the head node to be removed
-                if (size == 1) // If there's only one node
+                head = head.Next; // Move head to the next node
+                if (head != null) // If the list is not empty after removal
                 {
-                    head = null; // The list becomes empty
+                    head.Befor = null; // Update the new head's previous to null
                 }
-                else
-                {
-                    node tail = head;
-                    while (tail.next != head) // Find the last node
-                    {
-                        tail = tail.next;
-                    }
-                    tail.next = head.next; // Link last node to the second node
-                    head = head.next; // Move head to the next node
-                }
-                removedata = remove.Data; // Store the data of the removed node
-           
+                removedData = remove.Data; // Store the data of the removed node
             }
             else // If a node other than the head needs to be removed
             {
-                node newp = head;
-                for (int i = 0; i < index - 1; i++) // Move to the node before the index
+                Node current = head;
+                for (int i = 0; i < index; i++) // Move to the node at the specified index
                 {
-                    newp = newp.next;
+                    current = current.Next;
                 }
-                remove = newp.next; // Node to be removed
-                newp.next = remove.next; // Bypass the removed node
-                removedata = remove.Data; // Store the data of the removed node
+                remove = current; // Node to be removed
+                removedData = remove.Data; // Store the data of the removed node
+
+                // Bypass the removed node
+                if (remove.Next != null) // If it's not the last node
+                {
+                    remove.Next.Befor = remove.Befor; // Link the next node's befor to the previous node
+                }
+                if (remove.Befor != null) // If it's not the first node
+                {
+                    remove.Befor.Next = remove.Next; // Link the previous node's next to the next node
+                }
             }
 
             size--; // Decrement the size of the list
-            Console.WriteLine("removed data is: " + removedata); // Print the removed data
-            return removedata; // Return the data of the removed node
+            Console.WriteLine("Removed data is: " + removedData); // Print the removed data
+            return removedData; // Return the data of the removed node
         }
 
         // Method to remove a node at the end of the list
@@ -164,37 +164,8 @@ namespace linklist
             return size; // Return the size of the list
         }
 
-        // Method to invert the circular linked list
-        public void Invert()
-        {
-            if (head == null) return; // If the list is empty, do nothing
-
-            node prev = null; // Previous node
-            node current = head; // Current node
-            node next = null; // Next node
-            node tail = head; // To find the tail
-
-            // Find the tail node
-            while (tail.next != head)
-            {
-                tail = tail.next; // Move to the last node
-            }
-
-            // Invert the list
-            do
-            {
-                next = current.next; // Store the next node
-                current.next = prev; // Reverse the link
-                prev = current; // Move prev to current
-                current = next; // Move to the next node
-            } while (current != head);
-
-            head.next = prev; // Link the last node to the new head
-            head = prev; // Update head to the new head
-        }
-
         // Method to print the list
-        public void print()
+        public void Print()
         {
             if (head == null)
             {
@@ -202,45 +173,48 @@ namespace linklist
                 return;
             }
 
-            node newp = head; // Start from the head
-            do
+            Node current = head; // Start from the head
+            while (current != null) 
             {
-                Console.Write(newp.Data); // Print the data of each node
-                Console.Write(" -> "); // Indicate the link to the next node
-                newp = newp.next; // Move to the next node
-            } while (newp != head); // Continue until we loop back to the head
-            Console.WriteLine(" (back to head)"); // Indicate that we are back to the head
+                Console.Write(current.Data); // Print the data of each node
+                if (current.Next != null)
+                {
+                    Console.Write(" <-> "); 
+                }
+                current = current.Next; // Move to the next node
+            }
+            Console.WriteLine(); 
         }
     }
 
-    // Main program to test the circular linked list
+    // Main program to test the doubly linked list
     class Program
     {
         static void Main(string[] args)
         {
-            circlelinkedlist link = new circlelinkedlist(); // Create a new circular linked list
-            link.INsertAtIndex(1, 0); // Insert 1 at index 0
-            link.INsertAtIndex(2, 1); // Insert 2 at index 1
-            link.INsertAtIndex(3, 2); // Insert 3 at index 2
-            link.print(); // Display the list
+            DoublyLinkedList list = new DoublyLinkedList(); // Create a new doubly linked list
+            list.InsertAtIndex(1, 0); // Insert 1 at index 0
+            list.InsertAtIndex(2, 1); // Insert 2 at index 1
+            list.InsertAtIndex(3, 2); // Insert 3 at index 2
+            list.Print(); // Display the list
 
-            link.InsertAtBegin(0); // Insert 0 at the beginning
-            link.print(); // Display the list
+            list.InsertAtBegin(0); // Insert 0 at the beginning
+            list.Print(); // Display the list
 
-            link.InsertAtEnd(5); // Insert 5 at the end
-            link.print(); // Display the list
+            list.InsertAtEnd(4); // Insert 4 at the end
+            list.Print(); // Display the list
 
-            link.RemoveNodeAtBegin(); // Remove the head node
-            link.print(); // Display the list
+            list.RemoveNodeAtBegin(); // Remove the head node
+            list.Print(); // Display the list
 
-            link.RemoveNodeAtEnd(); // Remove the last node
-            link.print(); // Display the list
+            list.RemoveNodeAtEnd(); // Remove the last node
+            list.Print(); // Display the list
 
-            link.RemoveNodeAtIndex(2); // Remove the node at index 2
-            link.print(); // Display the list
+            list.RemoveNodeAtIndex(1); // Remove the node at index 1
+            list.Print(); // Display the list
 
-            link.Invert(); // Invert the circular linked list
-            link.print(); // Display the inverted list
+            list.UpdateNode(5, 0); // Update the first node's data to 5
+            list.Print(); // Display the list
         }
     }
 }
